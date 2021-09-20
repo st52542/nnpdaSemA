adee81817923828a23ab66db38768d439caa1d0f
st52542 <43936282+st52542@users.noreply.github.com>
2021-09-21 01:58:45 +0200

    add methods for UserRepository

--- src/main/java/upce/nnpda/sema/Repository/UserRepository.java
+++ src/main/java/upce/nnpda/sema/Repository/UserRepository.java
@@ -3,6 +3,20 @@ package upce.nnpda.sema.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import upce.nnpda.sema.Entity.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findById(Long id);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    User findByName(String jmeno);
+
+    User findByEmail(String email);
+
+    void removeUserById(Long id);
 }
