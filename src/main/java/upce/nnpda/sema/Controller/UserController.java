4240ae7559a8a15bda9d57f6170b615c62eae6e6
st52542 <43936282+st52542@users.noreply.github.com>
2021-10-03 01:19:58 +0200

    clean up

--- src/main/java/upce/nnpda/sema/Controller/UserController.java
+++ src/main/java/upce/nnpda/sema/Controller/UserController.java
@@ -3,48 +3,25 @@ package upce.nnpda.sema.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import upce.nnpda.sema.DTO.NewPasswordDTO;
 import upce.nnpda.sema.DTO.ChangePasswordDTO;
 import upce.nnpda.sema.DTO.ResetPasswordSendDTO;
 import upce.nnpda.sema.DTO.ResetPasswordTokenDTO;
-import upce.nnpda.sema.Entity.User;
-import upce.nnpda.sema.Repository.UserRepository;
-import upce.nnpda.sema.Security.jwt.JwtProvider;
 import upce.nnpda.sema.Service.UserService;
 import upce.nnpda.sema.DTO.LoginFormDTO;
 import upce.nnpda.sema.DTO.SignUpFormDTO;
-import upce.nnpda.sema.DTO.JwtResponseDTO;
-import upce.nnpda.sema.DTO.ResponseMessageDTO;
 
 
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
 import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
-
-    @Autowired
-    AuthenticationManager authenticationManager;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    PasswordEncoder encoder;
-
-    @Autowired
-    JwtProvider jwtProvider;
 
     @Autowired
     UserService userService;
